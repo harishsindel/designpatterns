29846460b6e1d800e9124dba6b460557c9868a78
Harish Sindel <harishsindel@gmail.com>
2023-06-16 14:51:12 +0530

    Adding empty test listner

---
+++ src/main/java/utils/TestListeners.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListeners implements ITestListener {
+	
+	public void onTestStart(ITestResult result) {
+		
+	}
+	
+
+}
